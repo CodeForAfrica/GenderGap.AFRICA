3a52f10907242bb5f39b77626a7dd0418341f4a9
Chafic Najjar <chafic.najjar@gmail.com>
2017-05-05 21:11:59 +0300

    Re-implement Twitter share button (#7)
    Re-implement Twitter share button to behave like the Facebook share
    button.

--- js/modules/social-media.js
+++ js/modules/social-media.js
@@ -1,15 +1,26 @@
 export default () => {
   let buttons = {
+    twitter: document.querySelector("#twitter-button"),
     facebook: document.querySelector("#facebook-button")
   };
 
-  buttons.facebook.addEventListener("click", function(event) {
+  buttons.twitter.addEventListener("click", (event) => {
     event.preventDefault();
 
-    let sharerURL = "https://facebook.com/sharer.php?u=" + encodeURIComponent("https://codeforafrica.github.io/GenderGapClock/dist/");
+    let url = "https://twitter.com/intent/tweet?text=...";
+    let name = "twitter-share-dialog";
+    let options = "menubar=no, toolbar=no, resizable=no, scrollbar=no, height=400, width=500";
+
+    window.open(url, name, options);
+  });
+
+  buttons.facebook.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let url = "https://facebook.com/sharer.php?u=" + encodeURIComponent("https://codeforafrica.github.io/GenderGapClock/dist/");
     let name = "facebook-share-dialog";
     let options = "menubar=no, toolbar=no, resizable=no, scrollbar=no, height=400, width=500";
 
-    window.open(sharerURL, name, options);
+    window.open(url, name, options);
   });
 };
